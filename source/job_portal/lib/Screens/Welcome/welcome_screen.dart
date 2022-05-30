28cfa1e9f9f19ab97f99539e0b06b3a2dd069c46
harshiniKumar <harshini.1701054@srec.ac.in>
2022-05-30 11:29:01 +0530

    welcome-screen-update

--- source/job_portal/lib/Screens/Welcome/welcome_screen.dart
+++ source/job_portal/lib/Screens/Welcome/welcome_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:job_portal/Screens/Welcome/components/body.dart';
+import 'package:job_portal/constants.dart';
 
 class WelcomeScreen extends StatelessWidget {
+  String dropdownValue = 'Option 1';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title:const Text('SHERO SEARCH' ),
+        backgroundColor: kPrimaryColor,
+        actions: [
+          PopupMenuButton(
+          icon: Icon(Icons.more_vert),
+          itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+            const PopupMenuItem(
+              child: ListTile(
+                title: Text('English'),
+              ),
+            ),
+            const PopupMenuItem(
+              child: ListTile(
+                title: Text('Tamil'),
+              ),
+            ),
+            const PopupMenuItem(
+              child: ListTile(
+                title: Text('Hindi'),
+              ),
+            ),
+        ],
+      )],
+      ),
       body: Body(),
     );
   }
